3154b86ec194d306b6a08ca4e7e64b2fc736070c
274664 <274664@LTIN109991.cts.com>
2019-09-05 17:16:17 +0530

    Deleted

--- SprintJwt/src/main/java/com/journaldev/spring/controller/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.journaldev.spring.controller;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
